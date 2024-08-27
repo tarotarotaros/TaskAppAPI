b5e79711140c24002fd011b848ab1facede0e329
tarotaros <y.t.tanpopo.15221@gmail.com>
2024-08-27 22:00:28 +0900

    [add]TEST用のAPI実装

--- TaskAppAPI/routes/api.php
+++ TaskAppAPI/routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::apiResource('test', TestController::class);
