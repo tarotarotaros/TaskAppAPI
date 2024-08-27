6dd30b23fe3bdc3876d46192dbbd7ade8997b4e8
tarotaros <y.t.tanpopo.15221@gmail.com>
2024-08-27 22:31:38 +0900

    [add]ログインメソッド

---
+++ TaskAppAPI/app/Http/Controllers/UserController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use \Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function register(Request $request)
+    {
+        // バリデーション
+        $validator = Validator::make($request->all(), [
+            'name' => ['required'],
+            'email' => ['required', 'email'],
+            'password' => ['required']
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->messages(), Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        // ユーザーを作成
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return response()->json('User registration completed', Response::HTTP_OK);
+    }
+
+
+    public function login(Request $request){
+        // バリデーション
+        $validator = Validator::make($request->all(), [
+            'email' => ['required', 'email'],
+            'password' => ['required']
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->messages(), Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        // ユーザー認証
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            $user = User::where('email', $request->email)->first();
+            $user->tokens()->delete();
+            $token = $user->createToken("login:user{$user->id}")->plainTextToken;
+
+            return response()->json(['token' => $token ], Response::HTTP_OK);
+        }
+
+        return response()->json('User unauthorized', Response::HTTP_UNAUTHORIZED);
+    }
+
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+        return response()->json(['message' => 'ログアウトしました。'], 200);
+    }
+        
+}
--- TaskAppAPI/routes/api.php
+++ TaskAppAPI/routes/api.php
@@ -3,10 +3,13 @@
 use Illuminate\Http\Request;
 use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
 
-
+Route::post('/user/login', [UserController::class, 'login']); // ログイン
+Route::post('/user/register', [UserController::class, 'register']); // ログイン
+Route::post('/user/logout', [UserController::class, 'logout']); // ログイン
 Route::apiResource('test', TestController::class);
